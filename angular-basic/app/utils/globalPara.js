ca2758ba29429459f89c8174be4f5b94599b2908
lesliewy <leslie_wangyang@163.com>
2017-08-16 07:50:18 +0800

    换新的ecs

--- angular-basic/app/utils/globalPara.js
+++ angular-basic/app/utils/globalPara.js
@@ -3,6 +3,12 @@ var GLOBAL = {
    tomcatAddr: 'http://123.57.50.82:8080',
    shinyAddr: 'http://123.57.50.82:3838'
    */
+
+   /*
+   tomcatAddr: 'http://59.110.235.115:8080',
+   shinyAddr: 'http://59.110.235.115:3838'
+   */
+
    /** dev begin **/
    tomcatAddr: 'http://127.0.0.1:8080',
    shinyAddr:  'http://127.0.0.1:3838'
